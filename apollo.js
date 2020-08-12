19b94f9aded84e66a255489079279efeba17ee25
leekyungho112 <epsoe@naver.com>
2020-08-13 00:20:30 +0900

    URL수정

--- apollo.js
+++ apollo.js
@@ -1,5 +1,7 @@
 const apolloClientOptions = {
-    uri: "http://192.168.0.7:4000"
+    uri: process.env.NODE_ENV ==="devalopment"
+    ? "http://192.168.0.7:4000"
+    : "https://keanghogram-backend.herokuapp.com/"
 };
 
 export default apolloClientOptions;
